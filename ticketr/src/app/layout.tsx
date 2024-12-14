10d6db6c6f7575052f5e64a67e497df0ac69c2c2
Sandeep Adhikari <adhikarisandeep321@gmail.com>
2024-12-14 13:02:36 -0600

    12/14/2024 1:01 PM

---
+++ ticketr/src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import Header from "@/components/Header";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Ticket App",
+  description: "Created by Snadeep using Clerk, Convex, Next JS, shadcnUI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ConvexClientProvider>
+          <ClerkProvider>
+            <Header/>
+          {children}
+          </ClerkProvider>
+        </ConvexClientProvider>
+        
+      </body>
+    </html>
+  );
+}
---
+++ ticketr/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className=''>
+      <h1>
+        Welcome to Ticketr
+      </h1>
+    </div>
+  );
+}
